9a09b088b42f69b978c7297616d92d08504daefe
Jayraj Pamnani <jayrajpamnani@Jayrajs-MacBook-Air.local>
2025-06-03 22:54:12 -0400

    fix: resolve deployment issues

--- next.config.js
+++ next.config.js
@@ -1,6 +1,5 @@
-import type { NextConfig } from "next";
-
-const nextConfig: NextConfig = {
+/** @type {import('next').NextConfig} */
+const nextConfig = {
   reactStrictMode: true,
   swcMinify: true,
   poweredByHeader: false,
@@ -61,4 +60,4 @@ const nextConfig: NextConfig = {
   },
 };
 
-export default nextConfig;
+module.exports = nextConfig;
